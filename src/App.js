b5905570244c6e52e2a36c539804dc3761e85e86
Maria Musto <mustom@bc.edu>
2026-03-10 20:11:58 -0400

    fix threshold dependency

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const useInView = (threshold = 0.15) => {
     const obs = new IntersectionObserver(([e]) => { if (e.isIntersecting) setInView(true); }, { threshold });
     if (ref.current) obs.observe(ref.current);
     return () => obs.disconnect();
-  }, []);
+  }, [threshold]);
   return [ref, inView];
 };
 
